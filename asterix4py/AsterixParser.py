662e5c06437db94c57441100d5a45df33a0ca0b2
Dan Landers <dan.landers@calanalytics.com>
2021-04-13 13:23:37 -0400

    Updated all asterix config files to latest versions included in asterix_decoder (https://github.com/CroatiaControlLtd/asterix/tree/master/asterix/config). Kept cat 242 config, which is missing in asterix_decoder. Updated config dict in AsterixParser. Added __init__.py file.

--- asterix4py/AsterixParser.py
+++ asterix4py/AsterixParser.py
@@ -15,23 +15,29 @@ dataItemsCache = {}
 uapItemsCache = {}
 
 astXmlFiles = {
-    1: 'asterix_cat001_1_1.xml',
+    1: 'asterix_cat001_1_2.xml',
     2: 'asterix_cat002_1_0.xml',
+    4: 'asterix_cat004_1_12.xml',
     8: 'asterix_cat008_1_0.xml',
     10: 'asterix_cat010_1_1.xml',
+    11: 'asterix_cat011_1_2.xml',
     19: 'asterix_cat019_1_2.xml',
     20: 'asterix_cat020_1_7.xml',
-    21: 'asterix_cat021_2_2.xml',
+    21: 'asterix_cat021_2_4.xml',
     23: 'asterix_cat023_1_2.xml',
+    25: 'asterix_cat025_1_2.xml',
     30: 'asterix_cat030_6_2.xml',
     31: 'asterix_cat031_6_2.xml',
     32: 'asterix_cat032_7_0.xml',
-    34: 'asterix_cat034_1_26.xml',
-    48: 'asterix_cat048_1_14.xml',
+    34: 'asterix_cat034_1_27.xml',
+    48: 'asterix_cat048_1_21.xml',
     62: 'asterix_cat062_1_18.xml',
     63: 'asterix_cat063_1_3.xml',
     65: 'asterix_cat065_1_3.xml',
+    205: 'asterix_cat205_1_0.xml',
+    240: 'asterix_cat240_1_3.xml',
     242: 'asterix_cat242_1_0.xml',
+    247: 'asterix_cat247_1_2.xml',
     252: 'asterix_cat252_7_0.xml'
 }
 
